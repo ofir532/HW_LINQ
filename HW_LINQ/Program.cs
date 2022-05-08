34584d6b358bf9998dd1a3d4cb0c5aef313d7074
Ofir Dahan <ofirxd532@gmail.com>
2022-05-08 22:50:23 +0300

    E

--- HW_LINQ/Program.cs
+++ HW_LINQ/Program.cs
@@ -26,7 +26,6 @@ namespace HW_LINQ
                 Console.WriteLine(list + ", ");
             }
 
-
             var results_ints1 = selectedNumbers.OrderBy(selectedNumbers => selectedNumbers);
             var results_ints2 = selectedNumbers.OrderByDescending(selectedNumbers => selectedNumbers);
             var results_strings1 = selectedStrings.OrderBy(selectedStrings => selectedStrings);
@@ -51,6 +50,29 @@ namespace HW_LINQ
                 Console.Write(list + ", ");
             }
             Console.WriteLine();
+
+            List<int> num1 = new List<int>() { 1455, 454232, 4543, 54544, 45545, 6 };
+            List<int> num2 = new List<int>() { 5431, 4542, 34554, 46532, 522, 6 };
+            var distinct = num1.Except(num2);
+            var intersect = num1.Intersect(num2);
+            var unionList = num1.Union(num2);
+            foreach (var list in distinct)
+            {
+                Console.Write(list + ", ");
+            }
+            Console.WriteLine();
+            foreach (var list in intersect)
+            {
+                Console.Write(list + ", ");
+            }
+            Console.WriteLine();
+            foreach (var list in unionList)
+            {
+                Console.Write(list + ", ");
+            }
+
+
+
 
         }
     }
